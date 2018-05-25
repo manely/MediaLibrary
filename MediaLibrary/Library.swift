67d685ad77f7d5d9d30f61718baa97de32b07c0b
Mani Ghasemnia Hamedani <mani.hamedani@me.com>
2018-05-25 13:08:42 +0430

    Draft version of Library.

--- MediaLibrary/Library.swift
+++ MediaLibrary/Library.swift
@@ -12,4 +12,86 @@ class Library {
     var name: String = ""
     var listOfPeople: [Person] = [Person]()
     var listOfBooks: [Book] = [Book]()
+    
+    var people: [Person] {
+        return listOfPeople
+    }
+    
+    var books: [Book] {
+        return listOfBooks
+    }
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func addBook(_ book: Book) {
+        //        guard !listOfBooks.contains(where: { b in
+        //            b.title == book.title
+        //        })
+        //        else {
+        //            return
+        //        }
+        listOfBooks.append(book)
+    }
+    
+    func addPerson(_ person: Person) {
+        listOfPeople.append(person)
+    }
+    
+    func removePerson(_ person: Person) {
+        let index = listOfPeople.index { p in
+            p.name == person.name
+        }
+        if (index != nil) {
+            listOfPeople.remove(at: index!)
+        }
+    }
+    
+    func removeBook(_ book: Book) {
+        let index = listOfBooks.index { b in
+            b.title == book.title
+        }
+        if (index != nil) {
+            listOfBooks.remove(at: index!)
+        }
+    }
+    
+    func checkOut(_ book: Book, to person: Person) {
+        let indexOfBook = listOfBooks.index { b in b.title == book.title }
+        let indexOfPerson = listOfPeople.index { p in p.name == person.name }
+        if indexOfBook != nil, indexOfPerson != nil {
+            var b = listOfBooks[indexOfBook!]
+            var p = listOfPeople[indexOfPerson!]
+            b.person = person
+            p.books.append(b)
+        }
+    }
+    
+    func checkIn(_ book: Book) {
+        if book.person == nil {
+            // book is not checked out before; unable to checkIn
+            return
+        }
+        let indexOfBook = listOfBooks.index { b in b.title == book.title }
+        if indexOfBook == nil {
+            // book is not in the library
+            return
+        }
+        var b = listOfBooks[indexOfBook!]
+        var p = b.person!
+        b.person = nil
+        if let indexOfBookInPersonsBooks = p.books.index(where: { b in b.title == book.title }) {
+            p.books.remove(at: indexOfBookInPersonsBooks)
+        }
+        
+    }
+    
+    func availableBooks() -> [Book] {
+        return listOfBooks.filter { $0.person == nil }
+    }
+    
+    func unAvailableBooks() -> [Book] {
+        return listOfBooks.filter { $0.person != nil }
+    }
 }
